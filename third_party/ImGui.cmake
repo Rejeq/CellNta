09cd3e11ba3579191afcf1bd5e014fb626e9651b
Rejeq <jere3690@gmail.com>
2022-12-04 02:35:30 +0000

    Used add_imgui_library for all imgui targets

--- third_party/ImGui.cmake
+++ third_party/ImGui.cmake
@@ -2,30 +2,41 @@ set(IMGUI_DIR ${CMAKE_CURRENT_LIST_DIR}/imgui)
 set(IMGUI_BACKEND_DIR ${IMGUI_DIR}/backends)
 set(IMGUI_MISC_DIR ${IMGUI_DIR}/misc)
 
-add_library(ImGui::ImGui INTERFACE IMPORTED)
-target_sources(ImGui::ImGui INTERFACE
+function(add_imgui_library TARGET)
+  cmake_parse_arguments(TARGET "" "" "HEADERS;SOURCES;DEPS" ${ARGN})
+
+  add_library(${TARGET} OBJECT)
+  add_library(ImGui::${TARGET} ALIAS ${TARGET})
+
+  target_sources(${TARGET} PRIVATE ${TARGET_SOURCES})
+  target_include_directories(${TARGET} PUBLIC ${TARGET_HEADERS})
+  target_link_libraries(${TARGET} PRIVATE ImGui::ImGui ${TARGET_DEPS})
+
+  # Prevent clang-tidy checks if enabled
+  set_target_properties(${TARGET} PROPERTIES CXX_CLANG_TIDY "")
+endfunction()
+
+add_imgui_library(ImGui SOURCES
   ${IMGUI_DIR}/imgui_demo.cpp
   ${IMGUI_DIR}/imgui_draw.cpp
   ${IMGUI_DIR}/imgui_tables.cpp
   ${IMGUI_DIR}/imgui_widgets.cpp
-  ${IMGUI_DIR}/imgui.cpp)
-target_include_directories(ImGui::ImGui INTERFACE
-  ${IMGUI_DIR})
+  ${IMGUI_DIR}/imgui.cpp
+  HEADERS ${IMGUI_DIR}
+)
 
-add_library(ImGui::ImplOpenGL3 INTERFACE IMPORTED)
-target_sources(ImGui::ImplOpenGL3 INTERFACE
-  ${IMGUI_BACKEND_DIR}/imgui_impl_opengl3.cpp)
-target_include_directories(ImGui::ImplOpenGL3 INTERFACE
-  ${IMGUI_BACKEND_DIR})
+add_imgui_library(ImplSDL2
+  SOURCES ${IMGUI_BACKEND_DIR}/imgui_impl_sdl.cpp
+  HEADERS ${IMGUI_BACKEND_DIR} ${SDL2_INCLUDE_DIRS}
+  DEPS ${SDL2_LIBRARIES}
+)
 
-add_library(ImGui::ImplSDL2 INTERFACE IMPORTED)
-target_sources(ImGui::ImplSDL2 INTERFACE
-  ${IMGUI_BACKEND_DIR}/imgui_impl_sdl.cpp)
-target_include_directories(ImGui::ImplSDL2 INTERFACE
-  ${IMGUI_BACKEND_DIR})
-
-add_library(ImGui::MiscCpp INTERFACE IMPORTED)
-target_sources(ImGui::MiscCpp INTERFACE
-    ${IMGUI_MISC_DIR}/cpp/imgui_stdlib.cpp)
-target_include_directories(ImGui::MiscCpp INTERFACE
-    ${IMGUI_MISC_DIR}/cpp)
+add_imgui_library(ImplOpenGL3
+  SOURCES ${IMGUI_BACKEND_DIR}/imgui_impl_opengl3.cpp
+  HEADERS ${IMGUI_BACKEND_DIR}
+  DEPS OpenGL::GL
+)
+add_imgui_library(MiscCpp
+  SOURCES ${IMGUI_MISC_DIR}/cpp/imgui_stdlib.cpp
+  HEADERS ${IMGUI_MISC_DIR}/cpp
+)
