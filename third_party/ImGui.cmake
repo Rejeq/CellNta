e5c474cff92118150781b8d707c0cf0b013af18a
Rejeq <jere3690@gmail.com>
2022-12-09 01:54:10 +0300

    Changed: One ImGui target so as not to clutter up the targetets

--- third_party/ImGui.cmake
+++ third_party/ImGui.cmake
@@ -2,19 +2,18 @@ set(IMGUI_DIR ${CMAKE_CURRENT_LIST_DIR}/imgui)
 set(IMGUI_BACKEND_DIR ${IMGUI_DIR}/backends)
 set(IMGUI_MISC_DIR ${IMGUI_DIR}/misc)
 
-function(add_imgui_library TARGET)
+function(add_imgui_library)
   cmake_parse_arguments(TARGET "" "" "HEADERS;SOURCES;DEPS" ${ARGN})
 
-  add_library(${TARGET} OBJECT)
-  add_library(ImGui::${TARGET} ALIAS ${TARGET})
-
-  target_sources(${TARGET} PRIVATE ${TARGET_SOURCES})
-  target_include_directories(${TARGET} PUBLIC ${TARGET_HEADERS})
-  target_link_libraries(${TARGET} PRIVATE ImGui::ImGui ${TARGET_DEPS})
-
-  # Prevent clang-tidy checks if enabled
-  set_target_properties(${TARGET} PROPERTIES CXX_CLANG_TIDY "")
+  target_sources(ImGui PRIVATE ${TARGET_SOURCES})
+  target_include_directories(ImGui PUBLIC ${TARGET_HEADERS})
+  target_link_libraries(ImGui PRIVATE ImGui::ImGui ${TARGET_DEPS})
 endfunction()
+
+add_library(ImGui OBJECT)
+add_library(ImGui::ImGui ALIAS ImGui)
+# Prevent clang-tidy checks if enabled
+set_target_properties(ImGui PROPERTIES CXX_CLANG_TIDY "")
 
 add_imgui_library(ImGui SOURCES
   ${IMGUI_DIR}/imgui_demo.cpp
@@ -25,18 +24,18 @@ add_imgui_library(ImGui SOURCES
   HEADERS ${IMGUI_DIR}
 )
 
-add_imgui_library(ImplSDL2
+add_imgui_library(
   SOURCES ${IMGUI_BACKEND_DIR}/imgui_impl_sdl.cpp
   HEADERS ${IMGUI_BACKEND_DIR} ${SDL2_INCLUDE_DIRS}
   DEPS ${SDL2_LIBRARIES}
 )
 
-add_imgui_library(ImplOpenGL3
+add_imgui_library(
   SOURCES ${IMGUI_BACKEND_DIR}/imgui_impl_opengl3.cpp
   HEADERS ${IMGUI_BACKEND_DIR}
   DEPS OpenGL::GL
 )
-add_imgui_library(MiscCpp
+add_imgui_library(
   SOURCES ${IMGUI_MISC_DIR}/cpp/imgui_stdlib.cpp
   HEADERS ${IMGUI_MISC_DIR}/cpp
 )
