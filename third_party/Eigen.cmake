8566a4454980da35280ba8c6a843085220253372
Rejeq <jere3690@gmail.com>
2023-01-25 01:46:25 +0300

    Changed: Removed third-party libraries folder

--- third_party/Eigen.cmake
+++
@@ -1,5 +0,0 @@
-add_library(Eigen3::Eigen INTERFACE IMPORTED)
-
-target_include_directories(Eigen3::Eigen INTERFACE
-  ${CMAKE_CURRENT_LIST_DIR}/Eigen
-)
--- third_party/Tracy.cmake
+++
@@ -1,8 +0,0 @@
-set(TRACY_DIR ${CELLNTA_THIRD_PARTY}/tracy)
-
-add_library(Tracy::Client INTERFACE IMPORTED)
-
-target_sources(Tracy::Client INTERFACE ${TRACY_DIR}/TracyClient.cpp)
-target_include_directories(Tracy::Client INTERFACE ${TRACY_DIR}/tracy)
-target_link_libraries(Tracy::Client INTERFACE ${CMAKE_DL_LIBS})
-target_compile_definitions(Tracy::Client INTERFACE TRACY_ENABLE)
