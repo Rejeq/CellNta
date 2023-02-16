9140c9430b23ea205182236ebe9d6f4b0636f10a
Rejeq <jere3690@gmail.com>
2023-02-16 15:51:50 +0300

    Added: Ability to choose Eigen building, because it takes a lot of memory

--- cmake/FindEigen.cmake
+++ cmake/FindEigen.cmake
@@ -1,5 +1,7 @@
 find_package(Eigen3 3.4.0 CONFIG)
 
+option(CELLNTA_THIRD_PARTY_Eigen3_BUILD "Enable Eigen building. When disabled just header only library will be added" OFF)
+
 if (NOT Eigen3_FOUND AND CELLNTA_FETCHING)
   set(EIGEN_URL "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz")
   message(STATUS "Loading Eigen3 from ${EIGEN_URL}")
@@ -10,7 +12,17 @@ if (NOT Eigen3_FOUND AND CELLNTA_FETCHING)
     DOWNLOAD_EXTRACT_TIMESTAMP ON
   )
 
-  FetchContent_MakeAvailable(eigen3)
+  if(CELLNTA_THIRD_PARTY_Eigen3_BUILD)
+    FetchContent_MakeAvailable(eigen3)
+  else()
+    FetchContent_GetProperties(eigen3)
+    if(NOT eigen3_POPULATED)
+      FetchContent_Populate(eigen3)
+      add_library(eigen INTERFACE IMPORTED)
+      add_library(Eigen3::Eigen ALIAS eigen)
+      target_include_directories(eigen INTERFACE "${eigen3_SOURCE_DIR}")
+    endif()
+  endif()
 
   message(STATUS "Eigen3 configured successfully")
 endif()
