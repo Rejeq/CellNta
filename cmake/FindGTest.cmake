8b21d283cb3515fbad29d44eb8abccadfceda240
Rejeq <jere3690@gmail.com>
2023-03-31 02:47:29 +0300

    Tests: Added googletest to CMake

---
+++ cmake/FindGTest.cmake
@@ -0,0 +1,17 @@
+list(REMOVE_ITEM CMAKE_MODULE_PATH "${CELLNTA_DIR}/cmake")
+find_package(GTest 1.11.0)
+list(APPEND CMAKE_MODULE_PATH "${CELLNTA_DIR}/cmake")
+
+if (NOT GTEST_FOUND AND CELLNTA_FETCHING)
+    set(GTEST_URL "https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz")
+    message(STATUS "Loading GTEST from ${GTEST_URL}")
+
+  include(FetchContent)
+  FetchContent_Declare(gtest
+    URL ${GTEST_URL}
+    DOWNLOAD_EXTRACT_TIMESTAMP ON
+  )
+
+FetchContent_MakeAvailable(gtest)
+  message(STATUS "GTEST configured successfully")
+endif()
