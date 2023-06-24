ade6552ac8f57380e94b3c87b8e7587015c1e297
Rejeq <jere3690@gmail.com>
2023-06-25 01:10:41 +0300

    Fixed: ImGui impl not configured by imgui_config.h

--- cmake/FindImGui.cmake
+++ cmake/FindImGui.cmake
@@ -17,18 +17,19 @@ set(IMGUI_MISC_DIR ${IMGUI_DIR}/misc)
 function(add_imgui_library)
   cmake_parse_arguments(TARGET "" "" "HEADERS;SOURCES;DEPS" ${ARGN})
 
-  target_sources(ImGui PRIVATE ${TARGET_SOURCES})
-  target_include_directories(ImGui PUBLIC ${TARGET_HEADERS})
-  target_link_libraries(ImGui PRIVATE ImGui::ImGui ${TARGET_DEPS})
+  target_sources(ImGui INTERFACE ${TARGET_SOURCES})
+  target_include_directories(ImGui INTERFACE ${TARGET_HEADERS})
+  target_link_libraries(ImGui INTERFACE ${TARGET_DEPS})
 endfunction()
 
-add_library(ImGui OBJECT)
+add_library(ImGui INTERFACE IMPORTED)
 add_library(ImGui::ImGui ALIAS ImGui)
 
 # Prevent clang-tidy checks if enabled
 set_target_properties(ImGui PROPERTIES CXX_CLANG_TIDY "")
 
-add_imgui_library(ImGui SOURCES
+add_imgui_library(
+  SOURCES
   ${IMGUI_DIR}/imgui_demo.cpp
   ${IMGUI_DIR}/imgui_draw.cpp
   ${IMGUI_DIR}/imgui_tables.cpp
