d06595f6148274f41c5eb59cb40adcfa6124635b
Rejeq <jere3690@gmail.com>
2023-07-28 11:27:10 +0300

    Fixed: Compile error when args is not constant

--- gui/Desktop/src/ActionBase.h
+++ gui/Desktop/src/ActionBase.h
@@ -49,15 +49,15 @@ inline BasePtr Make(Args&&... args) {
   return std::make_unique<ActionType>(std::forward<Args>(args)...);
 }
 
-// FMT_CONSTEVAL used because fmt::format_string can be not a consteval
+// FMT_HAS_CONSTEVAL used because fmt::format_string can be not a consteval
 #ifdef FMT_HAS_CONSTEVAL
-#define DK_CONSTEVAL FMT_CONSTEVAL
+#define DK_CONSTEXPR constexpr
 #else
-#define DK_CONSTEVAL inline
+#define DK_CONSTEXPR inline
 #endif
 
 template <typename... Args>
-DK_CONSTEVAL bool CheckLogErr(bool expr, fmt::format_string<Args...> fmt,
+DK_CONSTEXPR bool CheckLogErr(bool expr, fmt::format_string<Args...> fmt,
                               Args&&... args) {
   if (expr)
     DESKTOP_LOG_ERROR(fmt, std::forward<Args>(args)...);
@@ -65,7 +65,7 @@ DK_CONSTEVAL bool CheckLogErr(bool expr, fmt::format_string<Args...> fmt,
 }
 
 template <typename... Args>
-DK_CONSTEVAL bool CheckLogWarn(bool expr, fmt::format_string<Args...> fmt,
+DK_CONSTEXPR bool CheckLogWarn(bool expr, fmt::format_string<Args...> fmt,
                                Args&&... args) {
   if (expr)
     DESKTOP_LOG_WARN(fmt, std::forward<Args>(args)...);
@@ -75,15 +75,15 @@ DK_CONSTEVAL bool CheckLogWarn(bool expr, fmt::format_string<Args...> fmt,
 #define DESKTOP_ACTION_DERR_MSG "Unable to execute action "
 #define DESKTOP_ACTION_DERR_MSG_FMT DESKTOP_ACTION_DERR_MSG "{}"
 
-DK_CONSTEVAL bool CheckLogDErr(bool expr, const std::string_view& actionName) {
+DK_CONSTEXPR bool CheckLogDErr(bool expr, const std::string_view& actionName) {
   return CheckLogErr(expr, DESKTOP_ACTION_DERR_MSG_FMT, actionName);
 }
 
-DK_CONSTEVAL bool CheckLogDWarn(bool expr, const std::string_view& actionName) {
+DK_CONSTEXPR bool CheckLogDWarn(bool expr, const std::string_view& actionName) {
   return CheckLogWarn(expr, DESKTOP_ACTION_DERR_MSG_FMT, actionName);
 }
 
-#undef DK_CONSTEVAL
+#undef DK_CONSTEXPR
 
 }  // namespace Action
 }  // namespace Ui
